bf003d0a700e61ff8736face48169ad228b0e52d
luizottavio <contato.luizottavio@gmail.com>
2024-10-31 10:49:05 -0300

    Update

--- M03/ex/ex020.c
+++ M03/ex/ex020.c
@@ -4,9 +4,19 @@ int main() {
     printf("\nViagens até 200 km R$0.50/km");
     printf("\nA partir de 200 km R$0.35/km");
     printf("\n----------------------------");
-    float km;
 
+    float km;
     printf("\nDistância total de viagem, em km: ");
     scanf("%f", &km);
 
+    if (km <= 200) {
+        float preco = km * 0.50;
+        printf("Uma viagem de %.1f km vai custar R$0.50/km", km);
+        printf("\nValor total: R$%.2f\n", preco);
+    } else {
+        float preco = km * 0.35;
+        printf("Uma viagem de %.1f km vai custar R$0.35/km", km);
+        printf("\nValor total: R$%.2f\n", preco);
+    }
+    return 0;
 }
--- M03/ex/ex021.c
+++ M03/ex/ex021.c
@@ -5,11 +5,11 @@ void main() {
     printf("Digite um número: ");
     scanf("%i", &num);
 
-    if (num > 0) {
-        float inver = num/4;
+    if (num >= 0) {
+        float inver = num*num == 1;
         printf("O inverso de %i é igual a %.4f", num, inver);
     } else {
-        int opos = num + num;
+        int opos = num + num == 0;
         printf("O oposto de %i é igual a %i", num, opos);
     }
 }
